c19bf843bef99ae2d272a356c89daa0e81da24ef
Adarsh-oo7 <adarshsarachandran@gmail.com>
2024-10-14 22:11:09 +0530

    correcting the errors

--- reports/urls.py
+++ reports/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.revenue_generated,name="revenueGenerated")
+    path('',views.revenue_generated,name="revenueGenerated"),
+    path("viewCount",views.view_count,name="viewCount"),
+    path('subHistory',views.sub_history,name="subHistory"),
+    path('moveRating',views.movie_rating,name='moveRating')
+
 ]
--- reports/views.py
+++ reports/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def revenue_generated(request):
     return render(request,'reports/revenueGenerated.html')
+
+
+def view_count(request):
+    return render(request,'reports/viewCount.html')
+
+
+def sub_history(request):
+    return render(request,'reports/subHistory.html')
+
+
+def movie_rating(request):
+    return render(request,'reports/movieRating.html')
